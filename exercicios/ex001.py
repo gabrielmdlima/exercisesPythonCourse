df3bb4feee8f7ae4f3fbf841bdc16e548ae09573
gabrielmdlima <gabriel.mdlima@gmail.com>
2025-01-02 12:03:49 +0000

    Update ex001

--- exercicios/ex001.py
+++ exercicios/ex001.py
@@ -1,5 +1,6 @@
-from utils.utils import title
+from utils.utils import title, activity
 title()
+activity('Perguntar/Imprimir nome')
 
 nome = str(input('Qual é o seu nome? '))
 print(f'Olá, {nome}! Prazer em te conhecer!')
