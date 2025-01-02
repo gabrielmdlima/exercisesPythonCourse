c50e543ad6c849cee83fa1401bb049cb26073080
gabrielmdlima <gabriel.mdlima@gmail.com>
2025-01-02 12:05:13 +0000

    Update utils.py

--- exercicios/utils/utils.py
+++ exercicios/utils/utils.py
@@ -5,11 +5,11 @@ def title():
 	file = (__main__.__file__).split('\\')
 	if 'ex' in file[len(file) - 1]:
 		file = f' DESAFIO {file[len(file) - 1][2:5]} '
-		print(f'\033[32m{file:=^33}\033[m\n')
+		print(f'\033[32m{file:=^33}\033[m')
 	else:
 		print('\033[31m*title() não aplicável*\033[m')
 
 
 def activity(tarefa):
 	tarefa = str(' '+tarefa+' ').upper()
-	print(f'{tarefa:-^33}')
+	print(f'{tarefa:-^33}\n')
