1fdd26e263047a011a7e97ef33e3dbe8a79a8497
gabrielmdlima <gabriel.mdlima@gmail.com>
2025-01-02 11:47:47 +0000

    ex003

---
+++ exercicios/ex003.py
@@ -0,0 +1,7 @@
+from utils.utils import title, activity
+title()
+activity('Soma de inteiros')
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+print(f'A soma entre {n1} e {n2} é {n1 + n2}')
