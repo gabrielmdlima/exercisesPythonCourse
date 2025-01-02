7457a6f833c28c544931b189b5ceea2dc21b5636
gabrielmdlima <gabriel.mdlima@gmail.com>
2025-01-02 11:21:05 +0000

    Update utils.py

--- exercicios/utils/utils.py
+++ exercicios/utils/utils.py
@@ -1,7 +1,6 @@
 def title():
 	import __main__
 	file = (__main__.__file__).split('\\')
-	print(file)
 	if 'ex' in file[len(file) - 1]:
 		file = f' DESAFIO {file[len(file) - 1][2:5]} '
 		print(f'\033[32m{file:=^33}\033[m\n')
