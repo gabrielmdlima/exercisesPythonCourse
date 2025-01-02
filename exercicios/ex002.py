01f4f34a84d11245b1e643de3faa002be6c0a4a9
gabrielmdlima <gabriel.mdlima@gmail.com>
2025-01-02 12:03:56 +0000

    Update ex002

--- exercicios/ex002.py
+++ exercicios/ex002.py
@@ -1,5 +1,6 @@
-from utils.utils import title
+from utils.utils import title, activity
 title()
+activity('Data de nascimento')
 
 print(f'Qual a sua data de nascimento?')
 dia = int(input('Dia: '))
