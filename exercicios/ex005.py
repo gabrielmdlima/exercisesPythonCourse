31007427a9bffeec5f511450a6d85886d9754ba3
gabrielmdlima <gabriel.mdlima@gmail.com>
2025-01-02 12:09:22 +0000

    ex005

---
+++ exercicios/ex005.py
@@ -0,0 +1,6 @@
+from utils.utils import title, activity
+title()
+activity('Sucessor e antecessor')
+
+n = int(input('Digite um número: '))
+print(f'O número digitado foi {n}, seu antecessor é {n-1} e seu sucessor é {n+1}')
