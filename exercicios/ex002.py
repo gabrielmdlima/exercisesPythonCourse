c44b2b2ec88b4187c0a073343d309f17cfa026ec
gabrielmdlima <gabriel.mdlima@gmail.com>
2025-01-02 11:26:41 +0000

    Update utils.py

---
+++ exercicios/ex002.py
@@ -0,0 +1,8 @@
+from utils.utils import title
+title()
+
+print(f'Qual a sua data de nascimento?')
+dia = int(input('Dia: '))
+mes = input('Mês: ')
+ano = int(input('Ano: '))
+print(f'Você nasceu dia {dia}/{mes}/{ano}. Correto?')
--- exercicios/utils/utils.py
+++ exercicios/utils/utils.py
@@ -1,4 +1,6 @@
 def title():
+	from os import system
+	system('cls')
 	import __main__
 	file = (__main__.__file__).split('\\')
 	if 'ex' in file[len(file) - 1]:
@@ -6,3 +8,8 @@ def title():
 		print(f'\033[32m{file:=^33}\033[m\n')
 	else:
 		print('\033[31m*title() não aplicável*\033[m')
+
+
+def activity(tarefa):
+	tarefa = str(' '+tarefa+' ').upper()
+	print(f'{tarefa:-^33}')
