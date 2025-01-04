4c24e85fb1ae5d3a7e74b124fe9cc45e4550b76c
Gabriel Lima <gabriel.mdlima@gmail.com>
2025-01-04 18:56:04 +0000

    ex040

--- exercicios/ex040.py
+++ exercicios/ex040.py
@@ -1,3 +1,18 @@
 from utils.utils import title, activity
 title()
-activity()
+activity('Cálculo da média v2')
+
+score_amount = int(input('Quantas notas são? '))
+score = [float(input(f'Digite a {i+1}ª nota: ')) for i in range(score_amount)]
+total = 0
+for i in range(len(score)):
+  total += score[i]
+average = total / score_amount
+# print(f'A média das notas é {average:.1f}')
+
+if average < 5:
+  print(f'Sua média foi {average} e você está \033[31mREPROVADO!\033[m')
+elif 5 <= average <= 6.9:
+  print(f'Sua média foi {average} e você está de \033[33mRECUPERAÇÃO!\033[m')
+else:
+  print(f'Sua média foi {average} e você está \033[32mAPROVADO!\033[m PARABÉNS!')
