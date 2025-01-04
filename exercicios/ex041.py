c8a77037bd816f137a65befefeca17f34c48c0d6
Gabriel Lima <gabriel.mdlima@gmail.com>
2025-01-04 19:34:24 +0000

    Update ex041 (Classificação de atletas)

--- exercicios/ex041.py
+++ exercicios/ex041.py
@@ -5,7 +5,7 @@ activity('Classificação de atletas')
 from datetime import date
 
 CURRENT_YEAR = date.today().year
-YES = ['SIM', 'S', 'YES', 'Y']
+YES = ['SIM', 'S', 'YES']
 NO = ['NÃO', 'N', 'NO']
 
 def get_age():
@@ -16,24 +16,23 @@ def get_age():
       age = CURRENT_YEAR - birth_year
       print(f'\nVocê tem {age} anos')
       return age
-    elif birth_year >= CURRENT_YEAR:
-      print('\nA data de nascimento não pode ser no futuro!')
+    elif birth_year > CURRENT_YEAR:
+      print('\nA data de nascimento deve ser antes de 2025. Tente novamente.')
     else:
-      print('\nResposta inválida! Tente novamente.')
+      print('\nA data de nascimento não pode ser igual a 2025. Tente novamente.')
 
 
 def determine_category(age):
   if age <= 9:
-    category = 'MIRIM'
+    return 'MIRIM'
   elif 9 < age <= 14:
-    category = 'INFANTIL'
+    return 'INFANTIL'
   elif 14 < age <= 19:
-    category = 'JUNIOR'
+    return 'JUNIOR'
   elif age == 20:
-    category = 'SÊNIOR'
+    return 'SÊNIOR'
   else:
-    category = 'MASTER'
-  return category
+    return 'MASTER'
 
 
 def yes_or_no(prompt):
@@ -45,11 +44,11 @@ def yes_or_no(prompt):
       print('\nResposta inválida! Tente "Sim" ou "Não"')
 
 def classify_athlete():
-  print('='*32)
+  print('=' * 32)
   print('CONFEDERAÇÃO NACIONAL DE NATAÇÃO')
-  print('='*32)
-  if yes_or_no('\nVocê gostaria de se matricular na CNN:\n') in NO:
-    print('\nTenha um bom dia!')
+  print('=' * 32)
+  if yes_or_no('\nVocê gostaria de se matricular na Confederação Nacional de Natação?\n(Sim/Não): ') in NO:
+    print('\nObrigado por visitar nosso sistema! Tenha um bom dia!')
     return
   print('\nPrimeiro:')
   
@@ -57,5 +56,6 @@ def classify_athlete():
   category = determine_category(age)
 
   print(f'Sua categoria é: {category}')
+  print('\nObrigado por usar nosso sistema! Até logo!')
 
 classify_athlete()
