111a74dca51441d5315ff72bd2a421f397347f34
Gabriel Lima <gabriel.mdlima@gmail.com>
2025-01-05 19:10:22 +0000

    Update utils.py

--- exercicios/utils/utils.py
+++ exercicios/utils/utils.py
@@ -1,19 +1,19 @@
 def clear_screen():
-	from os import system, name
-	system('cls' if name == 'nt' else 'clear')
+  from os import system, name
+  system('cls' if name == 'nt' else 'clear')
 
 
 def title():
-	clear_screen()
-	import __main__
-	file = (__main__.__file__).split('\\')
-	if 'ex' in file[len(file) - 1]:
-		file = f' DESAFIO {file[len(file) - 1][2:5]} '
-		print(f'\033[32m{file:=^33}\033[m')
-	else:
-		print('\033[31m*title() não aplicável*\033[m')
+  clear_screen()
+  import __main__
+  file = (__main__.__file__).split('\\')
+  if 'ex' in file[len(file) - 1]:
+    file = f' DESAFIO {file[len(file) - 1][2:5]} '
+    print(f'\033[32m{file:=^33}\033[m')
+  else:
+    print('\033[31m*title() não aplicável*\033[m')
 
 
 def activity(tarefa):
-	tarefa = str(' '+tarefa+' ').upper()
-	print(f'{tarefa:-^33}\n')
+  tarefa = str(' '+tarefa+' ').upper()
+  print(f'{tarefa:-^33}\n')
