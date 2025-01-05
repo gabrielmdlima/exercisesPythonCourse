70d90e861f0d99fa96a7f58162f7d702933c436f
Gabriel Lima <gabriel.mdlima@gmail.com>
2025-01-05 00:45:49 +0000

    ex042

--- exercicios/ex042.py
+++ exercicios/ex042.py
@@ -1,3 +1,15 @@
 from utils.utils import title, activity
 title()
-activity()
+activity('Análise de trinângulos')
+
+s = [float(input(f'Digite o valor do {i+1}º segmento: ')) for i in range(3)]
+
+if s[0] < s[1] + s[2] and s[1] < s[0] + s[2] and s[2] < s[0] + s[1]:
+  if s[0] == s[1] and s[0] == s[2]:
+    print(f'\nOs segmentos de reta acima formam um triângulo EQUILÁTERO!')
+  elif s[0] == s[1] or s[1] == s[2] or s[2] == s[0]:
+    print(f'\nOs segmentos de reta acima formam um triângulo ISÓSCELES!')
+  else:
+    print(f'\nOs segmentos de reta acima formam um triângulo ESCALENO!')
+else:
+  print('Os segmementos acima NÃO PODEM formar um triângulo!')
