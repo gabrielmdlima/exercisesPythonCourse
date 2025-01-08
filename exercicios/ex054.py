8f531f4f34061f546d4e5e722b231e4224063176
Gabriel Lima <gabriel.mdlima@gmail.com>
2025-01-08 14:33:50 +0000

    Create ex054.py

---
+++ exercicios/ex054.py
@@ -0,0 +1,19 @@
+from utils.utils import title, activity
+title()
+activity('Verificação de idade')
+
+from datetime import date
+
+current_year = date.today().year
+birth_years = [0] * 7
+majority = 0
+minority = 0
+
+for i in range(7):
+  birth_years[i] = int(input(f'Digite ano de nascimento da {i+1}ª pessoa: '))
+  if (current_year - birth_years[i]) >= 21:
+    majority += 1
+  else:
+    minority += 1
+
+print(f'\nA quantidade de pessoas que não atingiu a maioridade é {minority} e que atingiram é {majority}')
