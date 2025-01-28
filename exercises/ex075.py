c17c43cf53b5d1933d92e1752ff3b98a366b2473
Gabriel Lima <gabriel.mdlima@gmail.com>
2025-01-28 20:59:27 +0000

    Correction ex075

--- exercises/ex075.py
+++ exercises/ex075.py
@@ -2,19 +2,18 @@ from utils.utils import title, activity
 title()
 activity('Análise de tupla')
 
-a = int(input('Digite um número: '))
-b = int(input('Digite outro número: '))
-c = int(input('Digite mais um número: '))
-d = int(input('Digite o último número: '))
-tupla = (a, b, c, d)
+numbers = (int(input('Digite um número: ')), 
+           int(input('Digite outro número: ')), 
+           int(input('Digite mais um número: ')), 
+           int(input('Digite o último número: ')))
 
-print(f'Você digitou os valores {tupla}')
-print(f'O valor 9 apareceu {tupla.count(9)}', 'vezes' if not tupla.count(9) == 1 else 'vez')
-if not 3 in tupla:
+print(f'Você digitou os valores {numbers}')
+print(f'O valor 9 apareceu {numbers.count(9)}', 'vezes' if not numbers.count(9) == 1 else 'vez')
+if not 3 in numbers:
   print(f'O valor 3 não foi digitado em nenhuma posição')
 else:
-  print(f'O valor 3 apareceu na {tupla.index(3)+1}ª posição')
+  print(f'O valor 3 apareceu na {numbers.index(3)+1}ª posição')
 print(f'Os valores pares digitados foram', end=' ')
-for par in tupla:
+for par in numbers:
   if par % 2 == 0:
     print(par, end=' ')
