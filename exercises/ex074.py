6ebf2b2d8062ca633904ff29a48d0c47804aedf7
Gabriel Lima <gabriel.mdlima@gmail.com>
2025-01-28 20:47:20 +0000

    Correction ex074

--- exercises/ex074.py
+++ exercises/ex074.py
@@ -5,22 +5,14 @@ activity('Números aleatórios')
 from random import randint
 
 random_numbers = (
-  randint(0, 9),
-  randint(0, 9),
-  randint(0, 9),
-  randint(0, 9),
-  randint(0, 9))
+  randint(1, 9),
+  randint(1, 9),
+  randint(1, 9),
+  randint(1, 9),
+  randint(1, 9))
 
 print('Os valores sorteados foram:', end=' ')
 for i, number in enumerate(random_numbers):
   print(f'{number}', end=' ')
-  if i == 0:
-    bigger = number
-    lower = number
-  if number > bigger:
-    bigger = number
-  elif number < lower:
-    lower = number
-print()
-print(f'O maior valor sorteado foi {bigger}')
-print(f'O menor valor sorteado foi {lower}')
+print(f'\nO maior valor sorteado foi {max(random_numbers)}')
+print(f'O menor valor sorteado foi {min(random_numbers)}')
