4213c61cf6e9100545691ef9fbabdd061dbad316
Gabriel Lima <gabriel.mdlima@gmail.com>
2025-02-06 16:18:52 +0000

    Correction ex080

--- exercises/ex080.py
+++ exercises/ex080.py
@@ -20,3 +20,24 @@ while len(num) < 5:
 
 print('-='*30)
 print(f'Os valores digitados em ordem foram {num}')
+input('\nPRESSIONE ENTER PARA IR PARA A CORREÇÃO...')
+title()
+activity('CORREÇÃO DO EXERCÍCIO')
+
+lista = []
+
+for c in range(0,5):
+  n = int(input('Digite um valor: '))
+  if c == 0 or n > lista[-1]:
+    lista.append(n)
+    print('Adicionado ao final da lista...')
+  else:
+    pos = 0
+    while pos < len(lista):
+      if n <= lista[pos]:
+        lista.insert(pos, n)
+        print(f'Adicionado na posição {pos} da lista...')
+        break
+      pos += 1
+print('-='*30)
+print(f'Os valores digitados em ordem foram {lista}')
