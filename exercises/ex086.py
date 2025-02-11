77b7ae3a1d01c7a0105358f89089f270077df406
Gabriel Lima <gabriel.mdlima@gmail.com>
2025-02-11 20:05:40 +0000

    ex086

---
+++ exercises/ex086.py
@@ -0,0 +1,15 @@
+from utils.utils import title, activity
+title()
+activity('Matriz')
+
+matriz = [[], [], []]
+
+for x in range(3):
+  for y in range(3):
+    matriz[x].append(int(input(f'Digite um valor para [{x}, {y}]: ')))
+print('-='*30)
+
+for linhas in matriz:
+  for nums in linhas:
+    print(f'[ {nums} ]', end='')
+  print()
